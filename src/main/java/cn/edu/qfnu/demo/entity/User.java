044cbdc182a0aa2b5cba619c49092b93550adf06
lgr56 <lgr56555@outlook.com>
2023-12-28 18:52:13 +0800

    主键自增

--- src/main/java/cn/edu/qfnu/demo/entity/User.java
+++ src/main/java/cn/edu/qfnu/demo/entity/User.java
@@ -1,9 +1,6 @@
 package cn.edu.qfnu.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -12,6 +9,7 @@ public class User {
      * 主键
      */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "f_id")
     private Integer id;
 
