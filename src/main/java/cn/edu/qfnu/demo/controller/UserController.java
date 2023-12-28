33ac6d9604b80a63eb30a99783ab28cf058d6e23
lgr56 <lgr56555@outlook.com>
2023-12-28 18:51:21 +0800

    分组查询

--- src/main/java/cn/edu/qfnu/demo/controller/UserController.java
+++ src/main/java/cn/edu/qfnu/demo/controller/UserController.java
@@ -2,8 +2,12 @@ package cn.edu.qfnu.demo.controller;
 
 import cn.edu.qfnu.demo.entity.User;
 import cn.edu.qfnu.demo.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -34,5 +38,13 @@ public class UserController {
     public  String removeById(@PathVariable Integer id){
         userRepository.deleteById(id);
         return "success";
+    }
+
+    @PostMapping("findList/{pageNum}/{pageSize}")
+    public List<User> findList(@PathVariable Integer pageNum, @PathVariable Integer pageSize){
+        PageRequest pageRequest = PageRequest.of(pageNum, pageSize);
+        Page<User> userPage = userRepository.findAll(pageRequest);
+
+        return userPage.stream().toList();
     }
 }
